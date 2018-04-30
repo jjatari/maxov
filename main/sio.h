91a7bd31bfcdd63b7e40dd8e8cafbcc650333d21
Jeff Thiele <jthiele.atari@gmail.com>
2018-04-29 23:09:01 -0500

    Decided to modify default UART driver to handle SIO traffic

--- main/sio.h
+++ main/sio.h
@@ -1,18 +1,6 @@
-#ifndef MINI850_SIO_H
-#define MINI850_SIO_H
+#ifndef MAXOV_SIO_H
+#define MAXOV_SIO_H
 
-#define SIO_BAUD_RATE      19200
-#define SIO_RX_BUF_SIZE    1024
-#define SIO_UART           (UART_NUM_2)
-
-typedef struct {
-  uint8_t device;
-  uint8_t command;
-  uint8_t aux1;
-  uint8_t aux2;
-} sio_command_t;
-
-esp_err_t sio_init();
-void sio_task();
+#define SIO_UART (UART_NUM_2)
 
 #endif
